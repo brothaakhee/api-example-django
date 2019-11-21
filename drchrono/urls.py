e0483a9c30b228e99dd4fce59c74315b56cea8c7
Hammad Hai <hammad.a.hai@gmail.com>
2019-11-21 08:38:36 -0500

    Create basic dashboard with vue

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -2,14 +2,21 @@ from django.conf.urls import include, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib import admin
-admin.autodiscover()
+
+from rest_framework.routers import DefaultRouter
 
 import views
 
+router = DefaultRouter()
+router.register(r'doctors', views.DoctorViewSet, basename='doctors')
+router.register(r'appointments', views.AppointmentViewSet, basename='appointments')
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^setup/$', views.SetupView.as_view(), name='setup'),
     url(r'^welcome/$', views.DoctorWelcome.as_view(), name='setup'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include((router.urls, 'api'))),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
 ]
--- drchrono/views.py
+++ drchrono/views.py
@@ -1,9 +1,19 @@
+import json
+
+from datetime import date
+
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from social_django.models import UserSocialAuth
 
-from drchrono.endpoints import DoctorEndpoint
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 
+from drchrono.endpoints import (
+    DoctorEndpoint,
+    PatientEndpoint,
+    AppointmentEndpoint
+)
 
 class SetupView(TemplateView):
     """
@@ -18,6 +28,10 @@ class DoctorWelcome(TemplateView):
     """
     template_name = 'doctor_welcome.html'
 
+    def __init__(self):
+        self.access_token = self.get_token()
+        return super(DoctorWelcome, self).__init__()
+
     def get_token(self):
         """
         Social Auth module is configured to store our access tokens. This dark magic will fetch it for us if we've
@@ -27,23 +41,107 @@ class DoctorWelcome(TemplateView):
         access_token = oauth_provider.extra_data['access_token']
         return access_token
 
-    def make_api_request(self):
+    def get_doctor(self):
         """
         Use the token we have stored in the DB to make an API request and get doctor details. If this succeeds, we've
         proved that the OAuth setup is working
         """
         # We can create an instance of an endpoint resource class, and use it to fetch details
-        access_token = self.get_token()
-        api = DoctorEndpoint(access_token)
+        api = DoctorEndpoint(self.access_token)
         # Grab the first doctor from the list; normally this would be the whole practice group, but your hackathon
         # account probably only has one doctor in it.
         return next(api.list())
+
+    def get_patients(self):
+        return PatientEndpoint(self.access_token).list()
+
+    def get_appointments(self):
+        return AppointmentEndpoint(self.access_token).list(date=date.today())
 
     def get_context_data(self, **kwargs):
         kwargs = super(DoctorWelcome, self).get_context_data(**kwargs)
         # Hit the API using one of the endpoints just to prove that we can
         # If this works, then your oAuth setup is working correctly.
-        doctor_details = self.make_api_request()
-        kwargs['doctor'] = doctor_details
+        #doctor_details = self.make_api_request()
+        kwargs['doctor'] = json.dumps(self.get_doctor())
+        kwargs['appointments'] = json.dumps(list(self.get_appointments()))
+        kwargs['patients'] = json.dumps(list(self.get_patients()))
+        kwargs['access_token'] = self.access_token
         return kwargs
 
+
+class DoctorViewSet(ViewSet):
+    """
+    ViewSet to wrap the DoctorEndpoint
+    """
+    def get_token(self):
+        """
+        Social Auth module is configured to store our access tokens. This dark magic will fetch it for us if we've
+        already signed in.
+        """
+        oauth_provider = UserSocialAuth.objects.get(provider='drchrono')
+        access_token = oauth_provider.extra_data['access_token']
+        return access_token
+
+    def list(self, request):
+        doctors = list(DoctorEndpoint(self.get_token()).list())
+        return Response(doctors)
+
+    def retrieve(self, request, pk=None):
+        doctor = DoctorEndpoint(self.get_token()).fetch(pk)
+        return Response(doctor)
+
+
+class PatientViewSet(ViewSet):
+    """
+    ViewSet to wrap the PatientEndpoint
+    """
+    def get_token(self):
+        """
+        Social Auth module is configured to store our access tokens. This dark magic will fetch it for us if we've
+        already signed in.
+        """
+        oauth_provider = UserSocialAuth.objects.get(provider='drchrono')
+        access_token = oauth_provider.extra_data['access_token']
+        return access_token
+
+    def list(self, request):
+        patients = list(PatientEndpoint(self.get_token()).list())
+        return Response(patients)
+
+    def retrieve(self, request, pk=None):
+        patient = PatientEndpoint(self.get_token()).fetch(pk)
+        return Response(patient)
+
+
+class AppointmentViewSet(ViewSet):
+    """
+    ViewSet to wrap the AppointmentEndpoint
+    """
+    def get_token(self):
+        """
+        Social Auth module is configured to store our access tokens. This dark magic will fetch it for us if we've
+        already signed in.
+        """
+        oauth_provider = UserSocialAuth.objects.get(provider='drchrono')
+        access_token = oauth_provider.extra_data['access_token']
+        return access_token
+
+    @property
+    def client(self):
+        return AppointmentEndpoint(self.get_token())
+
+    def list(self, request):
+        """
+        Only get todays appointments by default
+        """
+        todays_appointments = self.client.list(date=date.today())
+        return Response(list(todays_appointments))
+
+    def retrieve(self, request, pk):
+        appointment = self.client.fetch(pk)
+        return Response(appointment)
+
+    def update(self, request, pk):
+        appointment = self.client.update(pk, request.data)
+        return Response(appointment)
